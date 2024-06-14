8cd44a79ba3ca8d0869383f3c757a3526b6e5bee
Karl Ostendorf <kostend@twitch.tv>
2024-06-14 18:40:07 +0200

    add cursor control - wip

--- README.md
+++ README.md
@@ -18,3 +18,8 @@ gfa
 `gfa` is a javascript program, written in Typescript, built with `bun` yet targeting `node` environments. It is distributed as a single javascript file and can be run it you have node and git installed on your machine.
 
 To create the single javascript file: `bun run bundle`. The file will be localed at `./build/index.js`.
+
+## References
+
+- [mjs without extension](https://2ality.com/2022/07/nodejs-esm-shell-scripts.html#unix%3A-arbitrary-filename-extension-via-a-shell-prolog)
+- [original gfa](https://gist.github.com/kwo/bbd251ab1d3392ad95dc889948177a78)
--- index.ts
+++ index.ts
@@ -1,3 +1,55 @@
-const ansi = require('ansi-escape-sequences');
+import { exec } from 'child_process';
+import { promises as fs } from 'fs';
+import { promisify } from 'util';
+import ansi from 'ansi-escape-sequences';
 
-console.log(ansi.style.brightCyan + 'Hello via Bun!' + ansi.style.reset);
+const execPromise = promisify(exec);
+const lines: string[] = [];
+let linesUpdatedOnce: boolean = false;
+
+function updateLines() {
+  if (linesUpdatedOnce) {
+    console.log(ansi.cursor.previousLine(lines.length+1));
+  }
+  linesUpdatedOnce = true;
+  // console.log(ansi.erase.lines(lines.length));
+  console.log(lines.join('\n'));
+}
+
+function updateLine(lineNumber: number, newText: string) {
+  lines[lineNumber] = newText;
+  updateLines();
+}
+
+function appendToLine(lineNumber: number, extraText: string) {
+  const newText = `${lines[lineNumber]} ${extraText}`;
+  updateLine(lineNumber, newText);
+}
+
+async function process(lineNumber: number, dir: string) {
+  // branch
+  try {
+    const { stdout } = await execPromise('git branch --show-current', { cwd: dir });
+    const branch = stdout.trim();
+    appendToLine(lineNumber, branch);
+  } catch (x) {
+    appendToLine(lineNumber, `${ansi.style.red}${x}${ansi.style.reset}`);
+    return;
+  }
+}
+
+async function main(dir: string) {
+  const dirents = await fs.readdir(dir, { withFileTypes: true });
+  dirents
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+    .forEach(async (subdirName, i) => {
+      lines.push('');
+      updateLine(i, `${ansi.style.cyan}${subdirName}${ansi.style.reset}`);
+      await process(i, subdirName);
+    });
+}
+
+main('.')
+  .then(() => console.log('done'))
+  .catch(err => console.error(err));
