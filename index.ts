41cf37466ff8be4b73f02df88d2c9a0edf059a93
Karl Ostendorf <kostend@twitch.tv>
2024-06-14 20:55:55 +0200

    mvp achieved

--- index.ts
+++ index.ts
@@ -12,7 +12,9 @@ function updateLines() {
     console.log(ansi.cursor.previousLine(lines.length + 1));
   }
   linesUpdatedOnce = true;
-  console.log(lines.join('\n'));
+  for (const line of lines) {
+    console.log(`${ansi.erase.inLine(0)}${line}`);
+  }
 }
 
 function updateLine(lineNumber: number, text: string) {
@@ -25,14 +27,51 @@ function appendToLine(lineNumber: number, addedText: string) {
   updateLine(lineNumber, text);
 }
 
+// set the text of the last line
+function updateStatus(text: string) {
+  updateLine(lines.length - 1, text);
+}
+
 async function process(lineNumber: number, dir: string) {
   // branch
   try {
     const { stdout } = await execPromise('git branch --show-current', { cwd: dir });
     const branch = stdout.trim();
-    appendToLine(lineNumber, branch);
+    appendToLine(lineNumber, ` ${branch}`);
   } catch (x) {
-    appendToLine(lineNumber, `${ansi.style.red}${x}${ansi.style.reset}`);
+    appendToLine(lineNumber, ` ${ansi.style.red}${x}${ansi.style.reset}`);
+    return;
+  }
+
+  // fetch
+  try {
+    await execPromise('git fetch --all --tags --prune --prune-tags --quiet', { cwd: dir });
+    appendToLine(lineNumber, ' fetched');
+  } catch (x) {
+    appendToLine(lineNumber, ` ${ansi.style.red}${x}${ansi.style.reset}`);
+    return;
+  }
+
+  // clean
+  try {
+    const { stdout } = await execPromise('git status --porcelain', { cwd: dir });
+    if (stdout.trim().length === 0) {
+      appendToLine(lineNumber, ' clean');
+    } else {
+      appendToLine(lineNumber, ' dirty');
+      return;
+    }
+  } catch (x) {
+    appendToLine(lineNumber, ` ${ansi.style.red}${x}${ansi.style.reset}`);
+    return;
+  }
+
+  // pull
+  try {
+    await execPromise('git pull', { cwd: dir });
+    appendToLine(lineNumber, ' pulled');
+  } catch (x) {
+    appendToLine(lineNumber, ` ${ansi.style.red}${x}${ansi.style.reset}`);
     return;
   }
 }
@@ -41,16 +80,19 @@ async function main(dir: string) {
   const dirents = await fs.readdir(dir, { withFileTypes: true });
   const subdirs = dirents.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
 
-  subdirs.forEach((subdirName, i) => {
-    lines.push('');
-    updateLine(i, `${ansi.style.cyan}${subdirName}${ansi.style.reset}`);
+  subdirs.forEach(subdirName => {
+    lines.push(`${ansi.style.cyan}${subdirName}${ansi.style.reset}`);
   });
+  lines.push(''); // last line is status
 
-  subdirs.forEach(async (subdirName, i) => {
-    await process(i, subdirName);
+  updateStatus('working...');
+  const promises: Promise<void>[] = [];
+  subdirs.forEach((subdirName, i) => {
+    promises.push(process(i, subdirName));
   });
+  return Promise.all(promises);
 }
 
 main('.')
-  .then(() => console.log('done'))
-  .catch(err => console.error(err));
+  .then(() => updateStatus('done'))
+  .catch(err => updateStatus(err));
